645e95bb23ac1cbcda45f8e755dc3fcd6a840c6d
thejohnjohn <jp7480566@gmail.com>
2022-09-19 16:07:57 -0300

    chore: Add Client/Employee/Project modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmployeeModule } from './employee/employee.module';
+import { ClientModule } from './client/client.module';
+import { ProjectModule } from './project/project.module';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [EmployeeModule, ClientModule, ProjectModule],
 })
 export class AppModule {}
